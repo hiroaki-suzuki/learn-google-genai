88eb5f98059f699ebfd6564c66dc6635b8258001
Hiroaki Suzuki <rirazou05410@gmail.com>
2026-02-07 09:54:39 +0900

    DevContainerの設定を更新

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,3 +5,4 @@ sudo cp .devcontainer/config/.claude/* ~/.claude/
 sudo cp .devcontainer/config/.tmux.conf ~/
 
 sudo chown -R vscode:vscode /home/vscode/.claude /home/vscode/.tmux.conf
+sudo chmod +x /home/vscode/.claude/statusline-command.sh
